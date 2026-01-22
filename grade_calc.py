05a8db912b7b6d845763be803bffcbe08a31f9c8
Vedhanth2007 <vs396@kent.ac.uk>
2026-01-22 10:28:54 +0000

    made grade calc

---
+++ grade_calc.py
@@ -0,0 +1,13 @@
+grade = int(input("Enter your marks: "))
+if grade >=90 and grade <=100:
+    print("A")
+elif grade >= 80 and grade <= 89:
+    print("B")
+elif grade >= 70 and grade <= 79:
+    print("C")
+elif grade >= 60 and grade <= 69:
+    print("D")
+elif grade < 60 and grade >= 0:
+    print("F")
+else:
+    print("invalid input")
